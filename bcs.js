140e3635866a2a390c85021acacaff97214bc27e
Mika <mikadewitz@gmail.com>
2015-01-26 14:05:50 -1000

    adding 'play again' button

--- bcs.js
+++ bcs.js
@@ -1,13 +1,12 @@
-module.exports = {
-  computerChoice : function(bulbasaur, charmander, squirtle) {
-    var choice = [];
+// module.exports = {
+//   computerChoice : function(bulbasaur, charmander, squirtle) {
+//     var choice = [];
 
-    for (var i = 0; i < n; i++){
-      var randoChoice = Math.floor(Math.random()*arguments.length);
-      
-      }
-      choice.push(randoChoice);
-    }  
-    return choice;
-  }
-};
+//     for (var i = 0; i < n; i++){
+//       var randoChoice = Math.floor(Math.random()*arguments.length);
+//       choice.push(randoChoice);
+//       }
+//     }  
+//     return choice;
+//   }
+// };
