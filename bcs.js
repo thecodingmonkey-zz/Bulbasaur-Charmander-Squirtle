0313f383d4f0715a8ac21da970352d8f325d21b9
Mika <mikadewitz@gmail.com>
2015-01-26 14:35:16 -1000

    imputting computer, and player choice

--- bcs.js
+++ bcs.js
@@ -1,12 +1,62 @@
-// module.exports = {
-//   computerChoice : function(bulbasaur, charmander, squirtle) {
-//     var choice = [];
+function computerChoice() {
+  var choice = ["Bulbasaur", "Charmander", "Squirtle"];
+  var randoChoice = Math.floor(Math.random()*choice.length);
+  return choice[randoChoice];
+}
 
-//     for (var i = 0; i < n; i++){
-//       var randoChoice = Math.floor(Math.random()*arguments.length);
-//       choice.push(randoChoice);
-//       }
-//     }  
-//     return choice;
-//   }
-// };
+var playerPick;
+var computerPick;
+
+playerPick = window.location.search.substr(1);
+computerPick = computerChoice();
+
+var result;
+if (playerPick === "Bulbasaur" && computerPick === "Bulbasaur") {
+  result = "Tie";
+}
+if (playerPick === "Bulbasaur" && computerPick === "Charmander") {
+  result = "Computer Wins";
+}
+if (playerPick === "Bulbasaur" && computerPick === "Squirtle") {
+  result = "Player Wins";
+}
+
+if (playerPick === "Charmander" && computerPick === "Bulbasaur") {
+  result = "Player Wins";
+}
+if (playerPick === "Charmander" && computerPick === "Charmander") {
+  result = "Tie";
+}
+if (playerPick === "Charmander" && computerPick === "Squirtle") {
+  result = "Computer Wins";
+}
+
+if (playerPick === "Squirtle" && computerPick === "Bulbasaur") {
+  result = "Computer Wins";
+}
+if (playerPick === "Squirtle" && computerPick === "Charmander") {
+  result = "Player Wins";
+}
+if (playerPick === "Squirtle" && computerPick === "Squirtle") {
+  result = "Tie";
+}
+
+console.log(playerPick); 
+console.log(computerPick);
+console. log(result);
+
+
+
+
+
+
+
+var score=[];
+function point(){
+  score++;
+}
+function getScore(){
+  return score;
+//https://answers.yahoo.com/question/index?qid=20101208112148AAvahPD
+}
+
