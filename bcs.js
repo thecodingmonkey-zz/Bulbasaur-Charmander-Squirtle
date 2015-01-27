493d3bd9dd925459455826ee15538b91568ed3a9
Mika <mikadewitz@gmail.com>
2015-01-26 17:08:22 -1000

    adding score

--- bcs.js
+++ bcs.js
@@ -6,6 +6,18 @@ function computerChoice() {
 
 var playerPick;
 var computerPick;
+var winnerPic;
+
+var playerScore = window.name.playerScore;
+var computerScore = window.name.computerScore;
+
+if (playerScore === undefined) {
+  playerScore = 0;
+}
+if (computerScore === undefined) {
+  computerScore = 0;
+}
+
 
 playerPick = window.location.search.substr(1);
 computerPick = computerChoice();
@@ -13,33 +25,50 @@ computerPick = computerChoice();
 var result;
 if (playerPick === "Bulbasaur" && computerPick === "Bulbasaur") {
   result = "Tie";
+  winnerPic = "img/static-"+computerPick+".png";
+
 }
 if (playerPick === "Bulbasaur" && computerPick === "Charmander") {
   result = "Computer Wins";
+  winnerPic = "img/charmander.gif";
+  computerScore = computerScore+1;
 }
 if (playerPick === "Bulbasaur" && computerPick === "Squirtle") {
   result = "Player Wins";
+  winnerPic = "img/bulbasaur.gif";
+  playerScore = playerScore+1;
 }
 
 if (playerPick === "Charmander" && computerPick === "Bulbasaur") {
   result = "Player Wins";
+  winnerPic = "img/charmander.gif";
+  playerScore = playerScore+1;
 }
 if (playerPick === "Charmander" && computerPick === "Charmander") {
   result = "Tie";
 }
 if (playerPick === "Charmander" && computerPick === "Squirtle") {
   result = "Computer Wins";
+  winnerPic = "img/squirtle.gif";
+  computerScore = computerScore+1;
 }
 
 if (playerPick === "Squirtle" && computerPick === "Bulbasaur") {
   result = "Computer Wins";
+  winnerPic = "img/bulbasaur.gif";
+  computerScore = computerScore+1;
 }
 if (playerPick === "Squirtle" && computerPick === "Charmander") {
   result = "Player Wins";
+  winnerPic = "img/squirtle.gif";
+  playerScore = playerScore+1;
 }
 if (playerPick === "Squirtle" && computerPick === "Squirtle") {
   result = "Tie";
 }
+
+window.name.playerScore = playerScore;
+window.name.computerScore = computerScore;
 
 console.log(playerPick); 
 console.log(computerPick);
@@ -49,17 +78,15 @@ window.onload = function(){
   document.getElementById("Player_choice").innerHTML = playerPick;
   document.getElementById("Computer_choice").innerHTML = computerPick; 
   document.getElementById("BCS_result").innerHTML = result;
+  document.getElementById("playerPokemon").src="img/static-"+playerPick+".png";
+  document.getElementById("computerPokemon").src="img/static-"+computerPick+".png";
+  document.getElementById("winningPokemon").src=winnerPic;
+  document.getElementById("playerScore").innerHTML = playerScore;
+  document.getElementById("computerScore").innerHTML = computerScore;
 };
 
 
 
-
-var score=[];
-function point(){
-  score++;
-}
-function getScore(){
-  return score;
 //https://answers.yahoo.com/question/index?qid=20101208112148AAvahPD
-}
+
 
